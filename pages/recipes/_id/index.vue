4863ecca774d45098b11e76e081bd780ec0e5a7d
Nikolina1 <ljubicic.nikolina@gmail.com>
2020-04-10 19:18:36 +0200

    first commit

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,54 @@
+<template>
+    <section class="single-recipe">
+        <h1>{{ recipe.title }}</h1>
+        <div>
+            <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title"> 
+        </div>
+        <p>{{ recipe.descriptionText }}</p>
+    </section>
+</template>
+<script>
+export default {
+     asyncData(context) {
+        return new Promise ((resolve, reject) => {
+            setTimeout(() => {
+                resolve({
+                        recipe: [
+                        {
+                            id: "1",
+                            title: "Delicious Pizza",
+                            previewText: "Awesome Pizza!",
+                            descriptionText: "This is really delicious meal",
+                            thumbnail: "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg"
+                        },
+                        {
+                            id: "2",
+                            title: "Pasta",
+                            previewText: "Awesome Pasta!",
+                            descriptionText: "This is really delicious meal",
+                            thumbnail: "https://drop.ndtv.com/albums/COOKS/pasta-vegetarian/pastaveg_640x480.jpg"
+                        },
+                    ].find(el => el.id === context.params.id)
+                })
+            }, 1500)
+        })
+
+    }
+}
+</script>
+
+<style scoped>
+.single-recipe{
+    display:flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 30px;
+}
+
+.recipe-image{
+    width: 100px;
+    
+}
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,70 @@
+<template>
+    <section class="recipes">
+        <Recipe
+            v-for="recipe in recipes"
+            :key="recipe.id"
+            :thumbnail="recipe.thumbnail"
+            :title="recipe.title"
+            :previewText="recipe.previewText"/>        
+    </section>
+</template>
+
+
+
+<script>
+import Recipe from '@/components/Recipe'
+export default {
+    components: {
+        Recipe
+    },
+    asyncData() {
+        return new Promise ((resolve, reject) => {
+            setTimeout(() => {
+                resolve({
+                        recipes: [
+                        {
+                            id: "1",
+                            title: "Delicious Pizza",
+                            previewText: "Awesome Pizza!",
+                            thumbnail: "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg"
+                        },
+                        {
+                            id: "2",
+                            title: "Pasta",
+                            previewText: "Awesome Pasta!",
+                            thumbnail: "https://drop.ndtv.com/albums/COOKS/pasta-vegetarian/pastaveg_640x480.jpg"
+                        },
+                    ]
+                })
+            }, 1500)
+        })
+
+    }
+    
+};
+</script>
+
+<style scoped>
+.recipes{
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+}
+.recipe{
+    box-sizing: border-box;
+    width: 280px;
+    padding: 8px;
+    border: 1px solid #cccccc;
+    box-shadow: 0 2px 2px #aaaaaa;
+    margin: 10px;
+
+}
+
+.thumbnail{
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 200px;
+}
+</style>
